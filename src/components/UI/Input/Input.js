884ed9734a6a2a4afbc1f5902f276bcebda75920
najeesimmons <najee.bryant.simmons@gmail.com>
2022-06-22 16:03:03 -0400

    refactored input fields with forwardRefs

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,20 +1,37 @@
-import styles from './Input.module.css'
+import React, { useRef, useImperativeHandle } from 'react';
 
-const Input = props => {
-    return <div
-    className={`${styles.control} ${
-      props.isValid === false ? styles.invalid : ""
-    }`}
-  >
-    <label htmlFor={props.id}>{props.label}</label>
-    <input
-      type={props.type}
-      id={props.id}
-      value={props.value}
-      onChange={props.onChange}
-      onBlur={props.onBlur}
-    />
-  </div>
-};
+import styles from './Input.module.css';
 
-export default Input
+const Input = React.forwardRef((props, ref) => {
+  const inputRef = useRef();
+
+  const activate = () => {
+    inputRef.current.focus();
+  };
+
+  useImperativeHandle(ref, () => {
+    return {
+      focus: activate,
+    };
+  });
+
+  return (
+    <div
+      className={`${styles.control} ${
+        props.isValid === false ? styles.invalid : ''
+      }`}
+    >
+      <label htmlFor={props.id}>{props.label}</label>
+      <input
+        ref={inputRef}
+        type={props.type}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        onBlur={props.onBlur}
+      />
+    </div>
+  );
+});
+
+export default Input;
